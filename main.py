5d3dde720ffa269b8f9ccc36efcf9a50e65b92d7
haislanrafael <Haislanrafael1@gmail.com>
2025-04-01 13:09:22 -0300

    python-main

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+import random
+
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "hello word"}
+
+@app.get("/teste1")
+async def funcaoteste():
+    return {"teste": True, "deu certo": random.randint(0,1000)}
+
