8b13db2ce40193f97cd3a654869ff99e270ead51
haislanrafael <Haislanrafael1@gmail.com>
2025-04-25 22:43:23 -0300

    teest

---
+++ test/test_calculadora.py
@@ -0,0 +1,13 @@
+import unittest
+from calculadora import somar, subtrair
+
+class TestCalculadora(unittest.TestCase):
+    def test_somar(self):
+        self.assertEqual(somar(2, 3), 5)
+
+    def test_subtrair(self):
+        self.assertEqual(subtrair(5, 3), 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
